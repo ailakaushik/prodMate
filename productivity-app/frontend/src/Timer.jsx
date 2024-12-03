8d8c76d7ebe35fc09a7fd676e4c3023418b23f6e
Rockon <rockonthegamer@gmail.com>
2024-12-03 22:41:01 +0530

    added timer.jsx

---
+++ productivity-app/frontend/src/Timer.jsx
@@ -0,0 +1,13 @@
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+function Timer()
+{
+    return (
+        <>
+        <div><p>Timer</p></div>
+        </>
+    );
+}
+
+export default Timer
